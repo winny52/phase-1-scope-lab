2702501181c562048eb5a9e773c3155f92958e31
WinnySigilai <winnysigilai02@gmail.com>
2023-06-21 21:02:39 +0300

    Cpmpleted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    window.bestCustomer;
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer ="maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Winny";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Welle"; }
